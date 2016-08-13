7a3faf5638039a2d98faaec1f6d817986609d53f
yovie <yoviesmanda@gmail.com>
2016-08-13 07:46:54 +0700

    komit

--- wp-content/themes/startwordpress-master/index.php
+++ wp-content/themes/startwordpress-master/index.php
@@ -1,18 +1,94 @@
 <?php get_header(); ?>
-	<div class="row">
-		<div class="col-sm-8 blog-main">
-			<?php 
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_format() );
-			endwhile; ?>
-				<nav>
-					<ul class="pager">
-						<li><?php next_posts_link( 'Previous' ); ?></li>
-						<li><?php previous_posts_link( 'Next' ); ?></li>
-					</ul>
-				</nav>
-				<?php endif; ?>
-		</div>	<!-- /.blog-main -->
-		<?php get_sidebar(); ?>
+	<div class="row bariskahiji">		
+		<div class="col-md-7">
+			<h3>Tentang Kami</h3>
+			<p>Mang Koko yang menyandang nama asli  Haji Koko Koswara, lahir di Kecamatan Indihiang, Tasikmalaya, 10 April 1917 adalah seorang tokoh seniman Sunda. Siapapun yang menyukai kawih Sunda, dapat dipastikan mengenal atau sekurang-kurangnya mendengar nama: Mang Koko (alm). Beliau adalah seorang guru, santri, sastrawan, penulis, jurnalis, organisator, pencipta lagu, pembaharu karawitan Sunda, dan patut menjadi suri tauladan bagi praktisi seni di zaman sekarang. Mang Koko telah sukses mencipta kawih untuk anak-anak sampai tingkat dewasa, sehingga lebih dikenal sebagai maestro karawitan, dan karya-karyanya berupa kawih, tembang (pupuh rancag), sekar tandak, gending karesmen, dll., abadi sampai kiwari. Karya-karyanya masih bergema di panggung-panggung, radio, dan televisi.</p>
+			<span class="nextright"> <a href="">Selengkapnya &nbsp; &gt;&gt; </a></span>
+		</div>
+		<div class="col-md-5 text-center">
+			<img src="<?php echo get_template_directory_uri() ?>/img/image4.jpeg" />
+			<span>SMA Pasundan 3 Bandung dalam 60 detik</span>
+		</div>
 	</div> 	<!-- /.row -->
+
+	<div class="row bariskadua">
+		<h4>Apa kata mereka ?</h4>
+		<ul>
+			<li>
+				<img src="<?php echo get_template_directory_uri() ?>/img/image1.jpeg" />
+				<span class="papatah">"Ayah delapan anak ini sudah mencipta seribu lebih lagu pop Sunda. Badminton, yang populer itu – apalagi di saat bulu tangkis kita masih berjaya – ternyata diciptakan jauh sebelum Rudy Hartono menjuarai All England. Mang Koko menuliskan syair kocak itu tahun 1943, ketika ia masih membujang"</span>
+				<span class="nunulis">Bacharudin Jusuf Habibie</span>
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri() ?>/img/image1.jpeg" />
+				<span class="papatah">"Ayah delapan anak ini sudah mencipta seribu lebih lagu pop Sunda. Badminton, yang populer itu – apalagi di saat bulu tangkis kita masih berjaya – ternyata diciptakan jauh sebelum Rudy Hartono menjuarai All England. Mang Koko menuliskan syair kocak itu tahun 1943, ketika ia masih membujang"</span>
+				<span class="nunulis">Bacharudin Jusuf Habibie</span>
+			</li>
+		</ul>
+	</div>
+
+	<div class="row bariskatilu">
+		<div class="col-md-6">
+			<h4>Kegiatan</h4>
+			<ul class="kagiatan">
+				<li>
+					<div class="waktos">Jul 19</div>
+					<span class="nami">Kegiatan-kegiatan SMA Pasundan 3 Bandung </span> 
+					<span class="kagiatan">Pelestarian lingkungan hidup yang dikreasikan berupa : penghijauan, perbaikan selokan, perbaikan sarana Mandi Cuci Kakus (MCK) dan sebagainya;Mengadakan gerakan kebersihan lingkungan yang dikreasikan berupa kegiatan membersihkan coretan dinding, papan nama jalan, papan reklame, bus umum, gapura sekolah/gerbang sekolah</span> 
+				</li>
+				<li>
+					<div class="waktos">Jul 19</div>
+					<span class="nami">Kegiatan-kegiatan SMA Pasundan 3 Bandung </span> 
+					<span class="kagiatan">Pelestarian lingkungan hidup yang dikreasikan berupa : penghijauan, perbaikan selokan, perbaikan sarana Mandi Cuci Kakus (MCK) dan sebagainya;Mengadakan gerakan kebersihan lingkungan yang dikreasikan berupa kegiatan membersihkan coretan dinding, papan nama jalan, papan reklame, bus umum, gapura sekolah/gerbang sekolah</span> 
+				</li>
+				<li>
+					<div class="waktos">Jul 19</div>
+					<span class="nami">Kegiatan-kegiatan SMA Pasundan 3 Bandung </span> 
+					<span class="kagiatan">Pelestarian lingkungan hidup yang dikreasikan berupa : penghijauan, perbaikan selokan, perbaikan sarana Mandi Cuci Kakus (MCK) dan sebagainya;Mengadakan gerakan kebersihan lingkungan yang dikreasikan berupa kegiatan membersihkan coretan dinding, papan nama jalan, papan reklame, bus umum, gapura sekolah/gerbang sekolah</span> 
+				</li>
+			</ul>
+		</div>
+		<div class="col-md-6 backbiru">
+			<div class="col-md-12">
+				<h4>Berita Terkini</h4>
+				<div class="headline">
+					<img src="<?php echo get_template_directory_uri() ?>/img/image2.jpeg" />
+					<span class="judulna">Siswa SMA Pasundan 3 Bandung Mewakili Indonesia pada Olimpiade Kimia Internasional</span>
+					<span class="beritana">Pelestarian lingkungan hidup yang dikreasikan berupa : penghijauan, perbaikan selokan, perbaikan sarana Mandi Cuci Kakus (MCK) dan sebagainya;Mengadakan gerakan kebersihan lingkungan yang dikreasikan berupa kegiatan membersihkan coretan dinding, papan nama jalan, papan reklame, bus umum, gapura sekolah/gerbang sekolah</span>
+				</div>
+				<ul class="utama">
+					<li> <span class="dicon4"></span> <a href="">Kontrak Siswa dan Orang Tua dengan Sekolah</a> </li>
+					<li> <span class="dicon4"></span> <a href="">Festival Budaya Sunda dan Kelulusan SMA Pasundan 3 Bandung</a> </li>
+					<li> <span class="dicon4"></span> <a href="">Teater Senopati SMA Pasundan 3 Bandung Menjadi Duta Budaya</a> </li>
+				</ul>
+				<span class="nextright"> <a href=""> Selengkapnya &nbsp; &gt;&gt; </a></span>
+			</div>
+			<div class="col-md-12">
+				<h4>Galeri</h4>
+				<ul class="potopoto">
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/image1.jpeg" />
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/image2.jpeg" />
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/image3.jpeg" />
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri() ?>/img/image4.jpeg" />
+						</a>
+					</li>
+				</ul>
+				<span class="nextright"> <a href=""> Selengkapnya &nbsp; &gt;&gt; </a></span>
+			</div>
+		</div>
+	</div>
 <?php get_footer(); ?>
